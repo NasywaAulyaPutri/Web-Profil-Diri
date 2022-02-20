d690455edfdcbf5161a5a0a29931c2cfe6564f44
NasywaAulyaPutri <nasywaaulyaputri@smkwikrama.sch.id>
2022-02-20 17:21:19 +0700

    first commit

---
+++ Profildiri.php
@@ -0,0 +1,5 @@
+<div class="Halaman">
+	<h2>Halaman Depan</h2>
+	<p>Selamat datang di halaman beranda!</p>
+	<p>Perkenalkan Nama saya Nasywa Aulya Putri, Biasa di panggil Nasywa. Tanggal lahir saya 30 Oktober 2005, dan saat ini saya berusia 16 tahun. Hobi saya adalah membaca novel dan menonton film. Alamat Rumah saya Di jalan Tenjoayu Kecamatan Cicurug Kabupaten Sukabumi.</p>
+</div>
